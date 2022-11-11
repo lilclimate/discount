94ddc11e3eae03a496dbafad6a7352954f985af3
lil climate <lilclimate@gmail.com>
2022-11-12 02:47:50 +0800

    refactor: 封装计算

--- src/calc.ts
+++ src/calc.ts
@@ -1,16 +1,24 @@
-export  function calc(products, sets) {
-	const productsMap = products.reduce((acc, cur) => {
-		acc[cur.name] = cur.qty;
-		return acc;
-	 }, {});
-
-	const matchedSets = sets.reduce((acc, cur) => { 
-		const isMatched = cur.skus.every(sku =>  
-			productsMap[sku.name] && productsMap[sku.name] >= sku.qty
-		);	
-
-		if (isMatched) acc.push(cur); 
-		return acc;
-	}, []);
+export function calc(products, sets) {
+	const productsMap = parseProduct(products);
+	const matchedSets = getMatchedSets(sets, productsMap);
 	return matchedSets;	
 };
+
+function getMatchedSets(sets: any, products: any) {
+	return sets.reduce((acc, cur) => {
+		const isMatched = cur.skus.every(sku => products[sku.name] && products[sku.name] >= sku.qty
+		);
+
+		if (isMatched)
+			acc.push(cur);
+		return acc;
+	}, []);
+}
+
+function parseProduct(products: any) {
+	return products.reduce((acc, cur) => {
+		acc[cur.name] = cur.qty;
+		return acc;
+	}, {});
+}
+
