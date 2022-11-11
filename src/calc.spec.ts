f321b6bcbe7ae5ada601e4e92ccf75b5365c79fb
lil climate <lilclimate@gmail.com>
2022-11-12 01:52:48 +0800

    feat: 任务拆解

---
+++ src/calc.spec.ts
@@ -0,0 +1,13 @@
+import {test, expect } from "vitest";
+// TODO: 
+// happy path
+// single match with the same
+// single match with over sku type
+// single match with over sku qty
+// multiple matches and pick the best
+
+// sad path
+// not match when miss sku type
+// not match when sku qty not enough
+test.todo('single match with the same', () => { 
+});
