7e0f8d3e0593f1afa26c2acce7ed82464ae2904d
lil climate <lilclimate@gmail.com>
2022-11-18 01:38:50 +0800

    feat: 实现优惠套餐多方案筛选

--- src/plan.spec.ts
+++ src/plan.spec.ts
@@ -1,7 +1,118 @@
-import {test, expect } from "vitest";
+import dayjs from "dayjs";
+import {test, expect, vi } from "vitest";
+
+import { calc } from './plan';
 // Tasking:
-// TODO: 整合时间策略+商品策略
+// 整合时间策略+商品策略
 // 从多个方案中获取最佳商品套餐
 test('happy', () => { 
-	expect(1).toBe(1);
+	const productRules = [{
+	skus: [{
+		name: "sprite",
+		qty: 1
+	}, {
+		name: "cola",
+		qty: 1
+	}],
+	price: 1,
+},{
+	skus: [{
+		name: "water",
+		qty: 2
+	}, {
+		name: "cola",
+		qty: 1
+	}],
+	price: 2,
+},{
+	skus: [{
+		name: "sprite",
+		qty: 1
+	}, {
+		name: "cola",
+		qty: 1
+		}, {
+		name: "water",
+		qty: 1
+		}],
+	price: 1
+},{
+	skus: [{
+		name: "sprite",
+		qty: 2
+	}, {
+		name: "cola",
+		qty: 1
+	}],
+	price: 0,
+},{
+	skus: [{
+		name: "crisps",
+		qty: 2
+	}],
+	price: 3,
+}];
+
+	const timeRules = [{
+		dateRange: [1667232000, 1667663940], // 2022.11.01 00:00 - 2022.11.05 23:59
+		timeRange: ['21:00 - 03:00', '04:00 - 10:00', '11:00 - 15:00'],
+		week: [1, 2, 3, 4, 5, 6, 7],
+	}, {
+		dateRange: [1667491200, 1667923140], // 2022.11.04 00:00 - 2022.11.08 23:59
+		timeRange: ['21:00 - 03:00', '04:00 - 10:00', '11:00 - 15:00'],
+		week: [1, 2, 3, 4, 5, 6, 7],
+	}, {
+		dateRange: [1667232000, 1667491140], // 2022.11.01 00:00 - 2022.11.03 23:59
+		timeRange: ['21:00 - 03:00', '04:00 - 10:00', '11:00 - 15:00'],
+		week: [1, 2, 4, 5, 6, 7],
+	}];
+
+	const products = [{
+		name: "sprite",
+		qty: 2,
+		unitPrice: 1,
+	},
+	{
+		name: "cola",
+		qty: 1,
+		unitPrice: 1,
+	},
+	{
+		name: "water",
+		qty: 1,
+		unitPrice: 1,
+	},
+	{
+		name: "crisps",
+		qty: 2,
+		unitPrice: 1,
+	}
+	];
+
+	const repo = {
+		getPlans: vi.fn(() => [
+			{
+			id: 'A',
+			productRules: [productRules[2], productRules[3]],
+			timeRule: timeRules[2]
+		}, {
+			id: 'B',
+			productRules: [productRules[2], productRules[3]],
+			timeRule: timeRules[0]
+			},
+			{
+			id: 'C',
+			productRules: [productRules[4]],
+			timeRule: timeRules[0]
+		}]),
+	};
+	const currentTime = dayjs('2022.11.03 14:00').unix();
+	const data = calc(repo)(products, currentTime);
+	expect(data).toEqual({
+		totalPrice: 7,
+		discountSet :productRules[4],
+	});
 });
+
+
+
--- src/plan.ts
+++ src/plan.ts
@@ -1,3 +1,31 @@
-export function calc(params) {
-	
-};
+
+import lodash from 'lodash';
+import {calc as dateValid} from './dateTimeRange'
+import {calc as setValid  } from "./setMeal";
+export function calc(repo: any) { 
+	return (products, time: number) => {
+		let plans;
+		plans = repo.getPlans();
+		plans = (plans || []).filter(plan => dateValid([plan.timeRule], time));
+		const sets = (plans || []).map(plan => {
+			return setValid(products, plan.productRules)
+		 });
+		const data = pickBestSets(sets);
+		return data;
+	};
+}
+
+function pickBestSets(matchedSets: any) {
+	let set = null;
+	if (lodash.isEmpty(matchedSets)) return set;
+	if (matchedSets.length === 1) return matchedSets[0]; 
+
+	let maxAmount = 0;
+	return (matchedSets || []).reduce((acc, cur) => {
+		if (cur.totalPrice > maxAmount) {
+			maxAmount = cur.totalPrice;
+			acc = cur;
+		}
+		return acc;
+	}, set);
+}
