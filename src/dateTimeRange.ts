44dcfcd74e2b9a6322034d53369f0affe2e94d9e
lil climate <lilclimate@gmail.com>
2022-11-17 14:44:10 +0800

    refactor: 封装时间段对比

--- src/dateTimeRange.ts
+++ src/dateTimeRange.ts
@@ -9,18 +9,28 @@ export function calc(rules: any[], currentTime: number = now()) {
 };
 
 export function validDateRange(dateRange: number[], currentTime: number = now()) {
-	return dateRange[1] >= currentTime && dateRange[0] < currentTime;			
+	return validTmpTimeRange(dateRange, currentTime);
+
 };
 
 export function validTimeRange(timeRange: string, currentTime: number = now()) {
+	const tmpTimeRange = makeTimeRange(timeRange, currentTime);
+	return validTmpTimeRange(tmpTimeRange, currentTime);
+};
+
+function validTmpTimeRange(tmpTimeRange: any[], time: number) {
+	return tmpTimeRange[1] >= time && tmpTimeRange[0] < time;
+}
+
+function makeTimeRange(timeRange: string, time: number) {
 	const range = timeRange.split('-');
-	const date = dayjs(currentTime * 1000).format("YYYY.MM.DD");
+	const date = dayjs(time * 1000).format("YYYY.MM.DD");
 	const end = dayjs(`${date} ${range[1]}`).unix();
 	let start;
 	start = dayjs(`${date} ${range[0]}`).unix();
 	start = start > end ? start - 3600 * 24 : start;
-	return end >= currentTime && start < currentTime;
-};
+	return [start, end];
+}
 
 export function validWeek(days: number[], currentTime: number = now()) { 
 	return days.some(day => day === dayjs(currentTime * 1000).day());
